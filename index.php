16509b5b079382dcd5d2f879a6f3f42d88686cad
igorGun <grad6901@gmail.com>
2015-02-13 13:55:31 +0200

    Добавлено разделение разрядов

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
+<html>
+<head><title>Разряды</title></head>
+
+<body>
+<FORM  method="POST">
+ <INPUT NAME="num" VALUE ="<?php
+if (is_writable('number.txt')) {
+	echo file_get_contents('number.txt');
+}else{echo "";}
+
+ ?>" SIZE=20 MAXLEGNTH=20>
+  <p><input type="submit" value="Отправить">
+  
+ </FORM>
+ </body>
+
 <?php
-$a=2;
+$str=$_POST['num'];
+
+$a=preg_replace('/[^0-9]/','', $str);
+$format=number_format($a,0,'',' ');
+file_put_contents('number.txt', $format);
+
 
 ?>
+
+</html>
