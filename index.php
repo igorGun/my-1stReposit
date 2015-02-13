02bd19db344d58acb11a633a4c18e32d4c614e90
igorGun <grad6901@gmail.com>
2015-02-13 16:42:46 +0200

    finish

--- index.php
+++ index.php
@@ -3,19 +3,26 @@
 
 <body>
 	<?php
+
 $str=$_POST['num'];
 $a=preg_replace('/[^0-9]/','', $str);
-if ($a=="") {
-	$format="";
-}else{
-$format=number_format($a,0,'.',' ');}
+  if ($a=="") {
+	       if (file_get_contents('number.txt')!=0) {
+		   $format=file_get_contents('number.txt');
+	       }else{
+    $format="";
+   }}else{
+  $format=number_format($a,0,'.',' ');}
+
 file_put_contents('number.txt', $format);
+
+
 ?>
 <FORM  method="POST" >
- <INPUT NAME="num" VALUE ="<?php
-     if (is_readable('number.txt')) {
+ <INPUT NAME="num" VALUE ="<?php  
+  //   if (is_readable('number.txt')) {
 	    echo file_get_contents('number.txt');
-    }else{echo "";}
+  //  }else{echo "";}
 
     ?>" SIZE=20 MAXLEGNTH=20>
   <p><input type="submit" value="Отправить"> 
