dc385841da8c17969203afc56025eef66447248e
igorGun <grad6901@gmail.com>
2015-02-13 19:33:06 +0200

    rewrite

--- index.php
+++ index.php
@@ -9,10 +9,13 @@ $a=preg_replace('/[^0-9]/','', $str);
   if ($a=="") {
 	       if (file_get_contents('number.txt')!=0) {
 		   $format=file_get_contents('number.txt');
-	       }else{
-    $format="";
-   }}else{
-  $format=number_format($a,0,'.',' ');}
+	       }
+	       else{
+         $format="";
+                }
+ }else{
+  $format=number_format($a,0,'.',' ');
+      }
 
 file_put_contents('number.txt', $format);
 
@@ -21,7 +24,8 @@ file_put_contents('number.txt', $format);
 <FORM  method="POST" >
  <INPUT NAME="num" VALUE ="<?php  
   //   if (is_readable('number.txt')) {
-	    echo file_get_contents('number.txt');
+	    //echo file_get_contents('number.txt');
+            echo  $format;
   //  }else{echo "";}
 
     ?>" SIZE=20 MAXLEGNTH=20>
