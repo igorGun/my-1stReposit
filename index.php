f43a33a193168f4f180139d8f9f3bfac27067f9b
igorGun <grad6901@gmail.com>
2015-02-14 13:56:15 +0200

    rewrite6

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 
 <body>
 	<?php
+ if(isset($_POST['num']))
 
- 
-  if (file_exists('number.txt')) 
   {
-      if(isset($_POST['num']))
-      {
+       
 	     $str=$_POST['num'];
 	    
          $a=preg_replace('/[^0-9]/','', $str);
@@ -18,16 +16,16 @@
          }else{ $format=$a;
            // $format=number_format($a,0,'.',' ');
          }
-      }  else{
-       	 $format=file_get_contents('number.txt');
-       	 }
-  }else
-  {
-  	$format='';
-  }
-file_put_contents('number.txt', $format);
-
-
+         
+        
+       	 file_put_contents('number.txt', $format);
+  }else{
+         if (file_exists('number.txt')) {
+  	         $format=file_get_contents('number.txt');
+         }else{
+  	         $format='';
+         }
+ } 
 
 ?>
 <FORM  method="POST" >
@@ -37,7 +35,6 @@ echo number_format($format,0,'.',' ');
 }
        //   echo  $format;
   
-
     ?>" SIZE=20 MAXLEGNTH=20>
   <p><input type="submit" value="Отправить"> 
  </FORM>
